5035c0c9d2f5a3794689aa471b27bb0539859b0f
imbluedabr <baldergroen@gmail.com>
2026-01-08 00:29:26 +0100

    added new instructions

--- test_simd.vhd
+++ test_simd.vhd
@@ -15,7 +15,8 @@ entity ALU is
         mode : in std_logic_vector(1 downto 0);
         result : out std_logic_vector(N-1 downto 0);
         flags_in : in std_logic_vector(1 downto 0); -- 0: Z, 1: C
-        flags_out : out std_logic_vector(1 downto 0)
+        flags_out : out std_logic_vector(1 downto 0);
+        enable : in std_logic
     );
 
 end entity ALU;
@@ -35,7 +36,8 @@ begin
 
     add_res <= ('0' & unsigned(operand_a)) + b_unsigned + carry_in;
 
-    result <= std_logic_vector(add_res(N-1 downto 0));
+    result <= std_logic_vector(add_res(N-1 downto 0)) when enable = '1' else
+              operand_a;
     
     flags_out(0) <= '1' when add_res(N-1 downto 0) = to_unsigned(0, N) else
                     '0';
@@ -80,7 +82,8 @@ architecture SIMD_core_impl of SIMD_core is
             mode : in std_logic_vector(1 downto 0);
             result : out std_logic_vector(N-1 downto 0);
             flags_in : in std_logic_vector(1 downto 0);
-            flags_out : out std_logic_vector(1 downto 0)
+            flags_out : out std_logic_vector(1 downto 0);
+            enable : in std_logic
         );
 
     end component ALU;
@@ -114,7 +117,7 @@ architecture SIMD_core_impl of SIMD_core is
         alu_mode : std_logic_vector(1 downto 0);
         busrq : std_logic;
         write : std_logic;
-
+        set_ip : std_logic;
     end record;
     signal control_signals : control_t;
     type cycle_state is (FE, EX, WB);
@@ -124,8 +127,7 @@ architecture SIMD_core_impl of SIMD_core is
 begin
     
     --register file
-    reg_port1 <= (others => '0') when control_signals.en_alu else
-				 r1 when reg_select1 = "00" else
+    reg_port1 <= r1 when reg_select1 = "00" else
                  r2 when reg_select1 = "01" else
                  r3 when reg_select1 = "10" else
                  r4 when reg_select1 = "11" else
@@ -150,7 +152,15 @@ begin
                         operand when "11",
                         (others => '0') when others;
     alu0_mode <= control_signals.alu_mode;
-    ALU0: ALU port map(operand_a => reg_port1, operand_b => alu0_inputb, mode => alu0_mode, result => alu0_result, flags_in => alu0_flags, flags_out => alu0_newflags);
+    ALU0: ALU port map(
+        operand_a   => reg_port1,
+        operand_b   => alu0_inputb,
+        mode        => alu0_mode,
+        result      => alu0_result,
+        flags_in    => alu0_flags,
+        flags_out   => alu0_newflags,
+        enable      => control_signals.en_alu
+    );
 
     --data memory acces
     data_adres <= r4 & alu0_result; -- r4 is used as a banking register so we can acces more memory
@@ -172,14 +182,57 @@ begin
             sel_alu_inp => "00",
             alu_mode => "00",
             busrq => '0',
-            write => '0'
+            write => '0',
+            set_ip => '0'
         );
         case opcode(3 downto 0) is
-            when "0101" => --ld reg, operand
+            when "0000" => --add reg, reg
+                control_signals.set_reg <= '1';
+                control_signals.en_alu <= '1';
+                control_signals.sel_alu_inp <= "01";
+                control_signals.sel_reg_inp <= '1';
+                control_signals.alu_mode <= "00";
+            when "0001" => --addc reg, reg
+                control_signals.set_reg <= '1';
+                control_signals.en_alu <= '1';
+                control_signals.sel_alu_inp <= "01";
+                control_signals.sel_reg_inp <= '1';
+                control_signals.alu_mode <= "01";
+            when "0010" => --add reg, reg
+                control_signals.set_reg <= '1';
+                control_signals.en_alu <= '1';
+                control_signals.sel_alu_inp <= "01";
+                control_signals.sel_reg_inp <= '1';
+                control_signals.alu_mode <= "10";
+            when "0011" => --add reg, reg
+                control_signals.set_reg <= '1';
+                control_signals.en_alu <= '1';
+                control_signals.sel_alu_inp <= "01";
+                control_signals.sel_reg_inp <= '1';
+                control_signals.alu_mode <= "11";
+            when "0100" => --jmp operand
+                control_signals.set_ip <= '1';
+            when "1001" => --ld reg, reg(operand)
+                control_signals.set_reg <= '1';
+                control_signals.en_alu <= '1';
+                control_signals.sel_reg_inp <= '0';
+                control_signals.sel_alu_inp <= "11";
+                control_signals.busrq <= '1';
+            when "1010" => --ld reg, operand
                 control_signals.set_reg <= '1'; --set the register file in the writeback state
                 control_signals.en_alu <= '0'; --this just means that the alu will act like it doesnt exist when the mode is set to "00"
                 control_signals.sel_reg_inp <= '1'; --set the register file input to the alu
                 control_signals.sel_alu_inp <= "11"; --this sets input b of the alu to the immediate value
+            when "1011" => --mov reg, reg
+                control_signals.set_reg <= '1';
+                control_signals.en_alu <= '1';
+                control_signals.sel_reg_inp <= '1';
+                control_signals.sel_alu_inp <= "00";
+            when "1100" => --str reg, reg(operand)
+                control_signals.en_alu <= '1';
+                control_signals.sel_alu_inp <= "11";
+                control_signals.busrq <= '1';
+                control_signals.write <= '1';
             when others =>
                 null;
         end case;
@@ -219,6 +272,11 @@ begin
                         when "11" => r4 <= reg_input;
                     end case;
                 end if;
+
+                if control_signals.set_ip = '1' then
+                    instruction_pointer <= operand;
+                end if;
+                
                 cpu_state <= FE;
             end if;
 
